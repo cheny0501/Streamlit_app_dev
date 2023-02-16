395b1490b2292b0c1f54adf25b58e55d84c29210
cheny0501 <112567460+cheny0501@users.noreply.github.com>
2023-02-15 22:34:30 -0500

    final commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ ages = [
 heatmap = alt.Chart(subset).mark_rect().encode(
     x=alt.X("Age", sort=ages),
     y=alt.Y("Country"),
-    color=alt.Color("Rate", scale=alt.Scale(type='log',domain=(0.01,1000),clamp=True), title="Mortality rate per 100k"),
+    color=alt.Color("Rate", scale=alt.Scale(type='log',domain=(0.01,1000),clamp=True), legend=alt.Legend(title="Mortality rate per 100k")),
     tooltip=["Rate"],
 ).properties(
     title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
